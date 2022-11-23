77f7b40ca711e3c21fcbb5d7795b00ca7cc81026
ValentinDeOliveira <valentindeoliveira63@gmail.com>
2022-11-23 15:39:50 +0100

    removed useless imports

--- src/Main.py
+++ src/Main.py
@@ -1,5 +1,3 @@
-from scrapy.crawler import CrawlerProcess
-from jobs_parser.jobs_parser.spiders.JobParser import JobsParser
 from custom_parser.PageParser import PageParser
 
 import re
@@ -24,20 +22,6 @@ def main():
         cleanedUrls.append(cleanedUrl)
         
     print(res)
-    """
-    """
-    f = open("tmp.txt", "rb+")        
-    res = f.read()
-
-    ind = res.find('[[['.encode('UTF-8'))
-    completeData = res[ind:]
-    
-    urls = re.findall("https://.*\",{1}",completeData.decode("UTF-8"))
-    for encodedUrl in urls:
-        # remove useless '\'
-        cleanedUrl = re.sub('\\\(?![u])', '', encodedUrl[:-2])
-        # convert escaped unicode character
-        url = bytes(cleanedUrl, "utf-8").decode("unicode_escape")
     """
 if __name__ == "__main__":
     main()
