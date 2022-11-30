90c9dc4595ee0cefabb8d1ca5267f4b0fdcd98b5
ValentinDeOliveira <valentindeoliveira63@gmail.com>
2022-11-30 15:15:18 +0100

    Parsing a page is possible. For the moment we only get the description of the job and we save the data to a file

--- src/Main.py
+++ src/Main.py
@@ -8,20 +8,18 @@ def main():
     ]
     
     pageParser : PageParser = PageParser(jobs)
-    """
-    f = open("res3.txt", "r+")
-    res = f.read()
-    #res = bytes(f.read(), "utf-8").decode("unicode_escape")
-    urls = re.findall('(?P<url>https?://[^\s]+),', res)
+    
 
-    print(type(res))
+    """
+    f = open("res.txt", "r+")
+    res = bytes(f.read().replace(' ','').replace('\n',''), "utf-8").decode("unicode_escape")
+    res = bytes(res, "utf-8").decode("unicode_escape")
+    urls = re.findall("\"https?:\/\/[a-zA-Z0-9+%\-?=\\\/#_?&;.]*\"", res)
 
     cleanedUrls : list = list()
     for url in urls:
-        cleanedUrl = bytes(url[:-1], "utf-8").decode("unicode_escape")
+        cleanedUrl = bytes(url[1:-1], "utf-8").decode("unicode_escape")
         cleanedUrls.append(cleanedUrl)
-        
-    print(res)
-    """
+    """  
 if __name__ == "__main__":
     main()
--- src/custom_parser/PageParser.py
+++ src/custom_parser/PageParser.py
@@ -7,22 +7,29 @@ import requests
 import scrapy
 import re
 import urllib.request
+from selenium.webdriver.remote.webelement import WebElement
+from pyvirtualdisplay import display
 
 class PageParser:
     m_driver: webdriver.Chrome
 
     BASE_URL = 'https://www.google.com/search?q='
     BASE_SEARCH_OPTIONS = '&asearch=jb_list&cs=1&async=_id:VoQFxe,_pms:hts,_fmt:pc&rciv=jb&nfpr=0&start='
-
+    OFFER_SEARCH_OPTION = 'sclient=gws-wiz-serp&ibp=htl;jobs&sa=X#htivrt=jobs&fpstate=tldetail&htidocid='
     CLOSING_STYLE="</style>".encode('UTF-8')
 
     m_url = 'https://www.google.com/search?q=software+engineer+in+LA&sclient=gws-wiz-serp&ibp=htl;jobs&sa=X'
     def __init__(self, _jobs : list):
-        m_driver = webdriver.Chrome()
+        #m_display = display.Display(visible=0, size=(800, 800))  
+        #m_display.start()
+
+        self.m_driver = webdriver.Chrome()
         
-        m_driver.get(self.m_url)
+        self.m_driver.get(self.m_url)
         
-        title = m_driver.find_element(By.TAG_NAME, 'title').text
+        WebDriverWait(self.m_driver, timeout=10).until(lambda d: d.title != "")
+                
+        title = self.m_driver.title
         if title == 'Avant d\'accéder à la recherche Google':
             self.parseConsent(self.m_url)
         
@@ -43,41 +50,80 @@ class PageParser:
         """
         self.m_driver.get(_url)
         
-        WebDriverWait(self.m_driver, 20).until(EC.element_to_be_clickable((By.XPATH,'//*[@value="Tout accepter"]'))).click()
+        WebDriverWait(self.m_driver, 20).until(EC.element_to_be_clickable((By.XPATH,'//*[@jscontroller="soHxf"]'))).click()
 
-    def parse(self, _url: str):        
+    URL_ID="htidocid="
+    N_OFFERS_BLOCK=3
+
+    def parse(self, _url: str):   
+        # append the options needed for the search
         urlWithArgs = _url + self.BASE_SEARCH_OPTIONS
 
-        
-        for i in range(0, 3):
+        # get all the offers from 0 to 10*N_OFFERS_BLOCK
+        for i in range(0,self.N_OFFERS_BLOCK):
+            # add the index of offer block
             scrolledUrl = urlWithArgs + str(i * 10)
-            
+            # get the data from the url
             result = requests.get(scrolledUrl)
-                     
+            
+            # A lot of crap to remove, we will get the data after the </style>
             startingHTMLIndex = result.content.find(self.CLOSING_STYLE) + len(self.CLOSING_STYLE)
             decodedData  = result.content[startingHTMLIndex:].decode("unicode_escape")
+            # interesting data is at the end of the file and start by [[[
+            # we will find the index of those [[[ 
             ind = decodedData.find("[[[")
-            completeData = decodedData[ind:]
 
+            # remove spaces and \n, decode unicode characters
+            completeData = bytes(decodedData[ind:].replace(' ','').replace('\n',''), "utf-8").decode("unicode_escape")
+            # data is encoded twice with unicode, meaning that we need to decode data again
+            completeData = bytes(decodedData, "utf-8").decode("unicode_escape")
 
-            urls = re.findall('(?P<url>https?://[^\s]+),', completeData)
+            """
+                completeData contains JSON with a lot of unreadable data.
+                From this JSON we will get all the google's link.
+                Those link are link to a single offer
+            """
 
+            # get all the google links from the JSON
+            urls = re.findall("\"https:\/\/www.google.com[a-zA-Z0-9+%\-?=\\\/#_?&;.]*\"", completeData)
 
+            """
+                Foreach url:
+                    need to decode unicode characters from the URL
+                    get a particular id from this url
+                    append the parsed id to a different url that will redirect to a single offer
+            """
             for encodedUrl in urls:
                 # convert escaped unicode character
-                cleanedUrl = bytes(encodedUrl[:-1], "utf-8").decode("unicode_escape")
-                print(cleanedUrl)
+                cleanedUrl = bytes(encodedUrl[1:-1], "utf-8").decode("unicode_escape")
                 
-                self.parseOffer(cleanedUrl)
-                break
+                
+                urlIdPos=cleanedUrl.rfind(self.urlId)
+                urlId = cleanedUrl[urlIdPos + len(self.URL_ID):len(cleanedUrl)]
+                
+                finalUrl= _url + self.OFFER_SEARCH_OPTION + urlId
+                
+                self.parseOffer(finalUrl)
             break
             
         self.m_driver.close()
         
+    
+    RIGHT_CONTAINER = "jolnDe" 
+    OFFER_DESCRIPTION = "WbZuDe" 
+        
     def parseOffer(self, _url: str):
-        with urllib.request.urlopen(_url) as f:
-            data = f.read().decode('utf-8')
+        self.m_driver.get(_url)
 
-        soup : BeautifulSoup = BeautifulSoup(data)
-
-        pass
+        #container : WebElement = WebDriverWait(self.m_driver, timeout=10).until(lambda d: d.find_element(By.CLASS_NAME, self.RIGHT_CONTAINER))    
+        #descriptionContainer : WebElement= container.find_element(By.XPATH, './/span[@class=\"' + self.OFFER_DESCRIPTION + '\"]')
+        
+        # get container that holds the detailed description of the offer
+        descriptionContainer : WebElement= WebDriverWait(self.m_driver, timeout=10).until(lambda d: d.find_element(By.XPATH, './/span[@class=\"' + self.OFFER_DESCRIPTION + '\"]'))
+        
+        fullDescription = descriptionContainer.get_attribute('innerHTML')        
+        
+        f = open("resultOffer.txt", "a")
+        f.write(fullDescription)
+        f.write("\n-------------------------------------------------------------------------\n")
+        f.close()
